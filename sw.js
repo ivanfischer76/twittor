983ae92075d2ef8584c977df01f568ee44216963
Iván Fischer <ivanfischer76@gmail.com>
2021-03-16 12:40:56 -0300

    modificaciones para ios-16-03-2021

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -43,6 +43,9 @@ self.addEventListener('activate', e => {
   const respuesta = caches.keys().then(keys => {
     keys.forEach(key => {
       if (key !== STATIC_CACHE && key.includes('static')) {
+        return caches.delete(key);
+      }
+      if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
         return caches.delete(key);
       }
     });
