04f51fe729255da82b49c946f6fd3dad18578071
Iván Fischer <ivanfischer76@gmail.com>
2021-03-16 10:11:54 -0300

    corrección de error sintáctico

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var url = window.location.href;
 var swLocation = '/twittor/sw.js';
 
 if(navigator.serviceWorker) {
-  if(url.inculdes('localhost')){
+  if(url.includes('localhost')){
     swLocation = '/sw.js';
   }
   navigator.serviceWorker.register(swLocation);
